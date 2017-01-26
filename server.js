b0568c39729ca0c414165ca1d60c7358c860a59b
Scott Fox <scottfox5@gmail.com>
2017-01-26 16:12:08 -0600

    initial commit: set up files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var pets = require('./routes/pets');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/pets', pets);
+
+// serve the index page at /
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+  console.log('Listening on port ', server.address().port);
+});
